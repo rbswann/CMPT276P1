7577f8e1aabd4e43d314afad1c07659c9b01d761
yufengl <yufeng999@hotmail.com>
2020-11-04 19:44:35 -0800

    Registration activity
    Singleton created and to be used for Volley Requests
    
    SharedPreferencesUtility used to store data of user in application

---
+++ app/src/main/java/com/group04/studentaide/SharedPreferencesUtility.java
@@ -0,0 +1,85 @@
+package com.group04.studentaide;
+
+/*  This class is used to save key-data of the user, allowing them to not have to login everytime they open the app
+* Data is lost when: application is uninstalled or clearing application data through the settings
+*
+*
+* */
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+public class SharedPreferencesUtility {
+
+    //Shared preferences uses key's to retreive data that is stored in key-pair values
+    public static final String KEY_preferenceName = "userPrefrence";
+    public static final String KEY_firstName = "firstnamekey";
+    public static final String KEY_lastName = "lastnamekey";
+    public static final String KEY_email = "emailkey";
+
+    //Boolean -- needs to be set further down
+    public static final String KEY_educator ="educatorkey";
+
+    public static SharedPreferencesUtility mInstance;
+    public static Context mContext;
+
+    //Pass context of application
+    private SharedPreferencesUtility(Context context){
+        mContext = context;
+    }
+
+    //Synchronized makes it so that only one instance can execute at time -- preventing multiple user login's (?)
+    public static synchronized SharedPreferencesUtility getInstance(Context context){
+        if (mInstance == null){
+            mInstance = new SharedPreferencesUtility(context);
+        }
+        return mInstance;
+    }
+
+    /* Methods*/
+
+    //Save user credentials into shared preferences
+    public void userLogin(User user){
+        SharedPreferences sharedPreferences = mContext.getSharedPreferences(KEY_preferenceName, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(KEY_firstName, user.getFirstName());
+        editor.putString(KEY_lastName, user.getLastName());
+        editor.putString(KEY_email, user.getEmail());
+        editor.apply();
+    }
+
+    //Check if user is already logged in when opening application
+    //App uses email as username when logging in
+    //Checking against username that is stored in sharedPreferences
+    public boolean isLoggedIn() {
+        SharedPreferences sharedPreferences = mContext.getSharedPreferences(KEY_preferenceName, Context.MODE_PRIVATE);
+        return sharedPreferences.getString(KEY_email, null) != null;
+    }
+
+    //Retrieve logged in user
+    //For v2, will need to figure out how to save boolean for users to determine whether or not is student or educator
+    public User getUser(){
+        SharedPreferences sharedPreferences = mContext.getSharedPreferences(KEY_preferenceName, Context.MODE_PRIVATE);
+
+        return new User (
+                sharedPreferences.getString(KEY_firstName, null),
+                sharedPreferences.getString(KEY_lastName, null),
+                sharedPreferences.getString(KEY_email, null)
+        );
+
+    }
+
+    public void logout(){
+        SharedPreferences sharedPreferences = mContext.getSharedPreferences((KEY_preferenceName, Context.MODE_PRIVATE));
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        //Clear all saved details from our application context
+        editor.clear();
+        editor.apply();
+        mContext.startActivity(new Intent(mContext, LoginActivity.class))
+    }
+
+}
---
+++ app/src/main/java/com/group04/studentaide/StudentAideSingleton.java
@@ -0,0 +1,40 @@
+package com.group04.studentaide;
+
+
+//Singleton is used to create a single instance of our RequestQueue for the lifetime of our app
+//RequestQueue is used for our REST services such as GET and PUT and others
+//It will queue our requests and send them off one by one
+
+// REQUESTQUEUE MUS BE INSTANTIATED WITH THE APPLICATION CONTEXT ***NOT ACTIVIITY***
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class StudentAideSingleton {
+
+    private static StudentAideSingleton instance;
+    private RequestQueue mRequestQueue;
+    private static Context mContext;
+
+    private StudentAideSingleton(Context context){
+        mContext = context;
+        mRequestQueue = getRequestQueue();
+    }
+
+    public RequestQueue getRequestQueue() {
+
+        if (mRequestQueue == null){
+            //Using getApplicationContext prevents from leaking the Activity if something passes one in
+            mRequestQueue = Volley.newRequestQueue(mContext.getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request <T> request){
+        getRequestQueue().add(request);
+    }
+}
