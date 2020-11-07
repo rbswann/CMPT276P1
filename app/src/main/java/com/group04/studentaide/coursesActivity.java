834855532668c84b86ceb9484e30313299fa7a1e
yufengl <yufeng999@hotmail.com>
2020-11-07 12:54:49 -0800

    Course + study session

--- app/src/main/java/com/group04/studentaide/coursesActivity.java
+++ app/src/main/java/com/group04/studentaide/coursesActivity.java
@@ -5,18 +5,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 
 public class coursesActivity extends AppCompatActivity {
+
+    Button createCourseClicked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courses);
+
+        createCourseClicked.findViewById(R.id.courseCreate);
+
+        createCourseClicked.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                courseCreate();
+            }
+        });
+
     }
 
-    public void courseCreate(View view){
+    private void courseCreate(){
         Intent create = new Intent(this, courseCreation.class);
         startActivity(create);
     }
+
+    /*
+    To be completed in later versions
+
+    public void courseSelect(){
+
+    }
+
+    public void courseJoin(){
+
+    }
+     */
 }
