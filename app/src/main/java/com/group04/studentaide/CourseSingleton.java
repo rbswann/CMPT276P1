9eb8483809f123ac040e75bf9b39a9bdc932efac
Hareye <40867693+Hareye@users.noreply.github.com>
2020-11-09 13:00:49 -0800

    Update CourseSingleton.java

--- app/src/main/java/com/group04/studentaide/CourseSingleton.java
+++ app/src/main/java/com/group04/studentaide/CourseSingleton.java
@@ -2,9 +2,9 @@ package com.group04.studentaide;
 
 /*
 Written By: Yufeng Luo
+Edited By: Jason Leung
 Temporary fix to no database?
 Currently don't know how to split data for when a user wants to display study time from a certain period of time --> will need more research and adjust implementation
-
 This class is used to create a singleton object of our LHM and ArrayList,
 that way data can be shared throughout activities as the Instance is saved
  */
@@ -19,15 +19,15 @@ import java.util.Set;
 
 public class CourseSingleton{
     private static CourseSingleton ourInstance = null;
-    LinkedHashMap<String, Double> courseList;
+
+    LinkedHashMap<String, ArrayList<Double>> courseList;
+
     ArrayList<String> courseKeys;
 
     public CourseSingleton(){
-        courseList = new LinkedHashMap<String, Double>();
+        courseList = new LinkedHashMap<String,ArrayList<Double>>();
         //Dont know if can do this with arraylist and keys
         courseKeys = new ArrayList<String>();
-        courseKeys.add("General");
-        courseList.put("General", 0.0);
     }
 
     public static CourseSingleton getInstance(){
@@ -41,8 +41,10 @@ public class CourseSingleton{
     //When creating object
     //Courses courseList = new Courses();
     //Then to use Hashmap<String, Double> courseHM = courseList.getLinkedHM();
-    public LinkedHashMap<String, Double> getLinkedHM(){
+    public LinkedHashMap<String, ArrayList<Double>> getLinkedHM(){
+
         return courseList;
+
     }
 
 
@@ -50,29 +52,43 @@ public class CourseSingleton{
 
     //User enters a course name to create -- new k-v pair will be created in hashmap
     public void setCourseName(String courseName){
-        courseList.put(courseName, 0.0);
+
+        ArrayList<Double> temp = new ArrayList<Double>();
+        temp.add(0.0);
+        courseList.put(courseName, temp);
+
     }
 
     //Use this before adding new time studied to ensure that total time is updated correctly
-    public Double getStudyTime(String courseName) {
+    public ArrayList<Double> getStudyTime(String courseName) {
+
         return courseList.get(courseName);
+
     }
 
     //After a timer has run down and time has been calculated can add onto total
     public void setStudyTime(String courseName, double timeStudied){
-        courseList.put(courseName, timeStudied);
+
+        ArrayList<Double> temp = courseList.get(courseName);
+        temp.add(timeStudied);
+        courseList.put(courseName, temp);
+
     }
 
     public Boolean isEmpty(){
+
         return courseList.size() == 0;
+
     }
 
     public void setCourseKeys(String courseName){
+
         courseKeys.add(courseName);
+
     }
 
     //NO CLUE IF THIS WORKS -- there has to be a better way
-    public ArrayList<String> getKeys(LinkedHashMap<String, Double> LinkedHM, ArrayList<String> ArrayKeys){
+    public ArrayList<String> getKeys(LinkedHashMap<String, ArrayList<Double>> LinkedHM, ArrayList<String> ArrayKeys){
 
         Set<String> keySet = LinkedHM.keySet();
         ArrayKeys  = new ArrayList<String>(keySet);
