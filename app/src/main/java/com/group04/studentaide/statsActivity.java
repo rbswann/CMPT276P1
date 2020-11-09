6e20b654ee896b03b8cdf461906b95cb7c8d6812
Hareye <40867693+Hareye@users.noreply.github.com>
2020-11-09 13:01:39 -0800

    Update statsActivity.java

--- app/src/main/java/com/group04/studentaide/statsActivity.java
+++ app/src/main/java/com/group04/studentaide/statsActivity.java
@@ -1,224 +1,219 @@
 package com.group04.studentaide;
 
-import java.util.*;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 import android.os.Bundle;
 import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.json.JSONException;
-
-import static com.group04.studentaide.serverURL.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 /*  File Name: statsActivity.java
     Team: ProjectTeam04
     Written By: Jason Leung
-
     Changes:
         November 7th - Draft 1 of Version 1
         November 8th - Draft 2 of Version 1
-
+        November 9th - Finalized Version 1
     Bugs:
-        Haven't been tested yet.
+
  */
 
-public class statsActivity extends AppCompatActivity {
+public class studyStatistics extends AppCompatActivity {
 
-    TextView totalTimeStudied;
+	TextView totalTimeStudied;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stats);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_stats);
 
-        double totalTime = getTimeTotalNoServer();
-        String displayTotal = Double.toString(totalTime);
+		double totalTime = getTimeTotalNoServer();
+		String displayTotal = Double.toString(totalTime);
 
-        totalTimeStudied = (TextView) findViewById(R.id.statslabel);
-        totalTimeStudied.setText(displayTotal);
+		totalTimeStudied = (TextView) findViewById(R.id.totalTimeCount);
+		totalTimeStudied.setText(displayTotal);
 
-    }
+	}
 
-    // Tested with Alex, it works
-    private double getTimeTotalNoServer() {
+	private double getTimeTotalNoServer() {
 
-        CourseSingleton courseList = CourseSingleton.getInstance();
-        LinkedHashMap<String, Double> courseHM = courseList.getLinkedHM();
-        double totalTime = 0;
+		CourseSingleton courseList = CourseSingleton.getInstance();
+		LinkedHashMap<String, ArrayList<Double>> courseHM = courseList.getLinkedHM();
 
-        ArrayList<String> keys = new ArrayList<String>();
-        keys = courseList.getKeys(courseHM, keys);
-        double courseTime;
+		double totalTime = 0;
 
-        for (int i = 0; i < keys.size(); i++) {
+		ArrayList<String> courseNames = new ArrayList<String>();
+		courseNames = courseList.getKeys(courseHM, courseNames);
+		ArrayList<Double> allTimeStudied;
 
-            courseTime = courseList.getStudyTime(keys.get(i));
-            totalTime += courseTime;
+		for (int i = 0; i < courseNames.size(); i++) {
 
-        }
+			allTimeStudied = courseList.getStudyTime(courseNames.get(i));
 
-        return totalTime;
+			for (int j = 0; j < allTimeStudied.size(); j++) {
 
-    }
+				totalTime += allTimeStudied.get(j);
 
-    /* GET request to retrieve all study session data and returns sum of all duration (totalTimeStudied).
+			}
+
+		}
+
+		return totalTime;
+
+	}
+
+	/* GET request to retrieve all study session data and returns sum of all duration (totalTimeStudied).
        Currently requires database to function, will change once we get internal storage working.
      */
-    private double getTimeTotal(String GUID) throws Exception {
 
-        String getUrl = ROOT_URL + "study_schedule/" + GUID;
-        ArrayList<Double> durationList = new ArrayList<>();
-        
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, getUrl, null, new Response.Listener<JSONObject>() {
+	/*
+	private double getTimeTotal(String GUID) throws Exception {
 
-            @Override
-            public void onResponse(JSONObject response) {
+		String getUrl = ROOT_URL + "study_schedule/" + GUID;
+		ArrayList<Double> durationList = new ArrayList<>();
 
-                try {
+		JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, getUrl, null, new Response.Listener<JSONObject>() {
 
-                    JSONArray jsonArray = response.getJSONArray("study_schedule");
+			@Override
+			public void onResponse(JSONObject response) {
 
-                    for (int i = 0; i < jsonArray.length(); i++) {
+				try {
 
-                        JSONArray jsonArr2 = jsonArray.getJSONArray(i);
+					JSONArray jsonArray = response.getJSONArray("study_schedule");
 
-                        for (int j = 0; j < jsonArr2.length(); j++) {
+					for (int i = 0; i < jsonArray.length(); i++) {
 
-                            JSONObject jsonObject = jsonArr2.getJSONObject(j);
-                            double duration = jsonObject.getDouble("duration");
+						JSONArray jsonArr2 = jsonArray.getJSONArray(i);
 
-                            String start = jsonObject.getString("start");
-                            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-                            LocalDateTime startDateTime = LocalDateTime.parse(start, formatter);
-                            LocalDateTime currentDateTime = LocalDateTime.now();
+						for (int j = 0; j < jsonArr2.length(); j++) {
 
-                            if (currentDateTime.isAfter(startDateTime)) {
+							JSONObject jsonObject = jsonArr2.getJSONObject(j);
+							double duration = jsonObject.getDouble("duration");
 
-                                durationList.add(duration);
+							String start = jsonObject.getString("start");
+							DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+							LocalDateTime startDateTime = LocalDateTime.parse(start, formatter);
+							LocalDateTime currentDateTime = LocalDateTime.now();
 
-                            }
+							if (currentDateTime.isAfter(startDateTime)) {
 
-                        }
+								durationList.add(duration);
 
-                    }
+							}
 
-                } catch (JSONException e) {
+						}
 
-                    e.printStackTrace();
+					}
 
-                }
+				} catch (JSONException e) {
 
-            }
+					e.printStackTrace();
 
-        }, new Response.ErrorListener() {
+				}
 
-            @Override
-            public void onErrorResponse(VolleyError error) {
+			}
 
-                error.printStackTrace();
+		}, new Response.ErrorListener() {
 
-            }
+			@Override
+			public void onErrorResponse(VolleyError error) {
 
-        });
+				error.printStackTrace();
 
-        double totalTime = 0;
+			}
 
-        for (int i = 0; i < durationList.size(); i++) {
+		});
 
-            totalTime += durationList.get(i);
+		double totalTime = 0;
 
-        }
+		for (int i = 0; i < durationList.size(); i++) {
 
-        StudentAideSingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
-        return totalTime;
+			totalTime += durationList.get(i);
 
-    }
+		}
 
-    /* GET request to retrieve all study session data and only returns sum of duration from specific courses
+		StudentAideSingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+		return totalTime;
+
+	}
+	*/
+	/* GET request to retrieve all study session data and only returns sum of duration from specific courses
        (courseTime).
        Currently requires database to function, I don't think we'll be using this in v1 so I won't be
        editing it for now.
      */
-    private double getTimeCourse(String GUID, int courseCode) throws Exception {
+	/*
+	private double getTimeCourse(String GUID, int courseCode) throws Exception {
 
-        String getUrl = ROOT_URL + "study_schedule/" + GUID;
-        ArrayList<Double> courseDurationList = new ArrayList<>();
+		String getUrl = ROOT_URL + "study_schedule/" + GUID;
+		ArrayList<Double> courseDurationList = new ArrayList<>();
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, getUrl, null, new Response.Listener<JSONObject>() {
+		JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, getUrl, null, new Response.Listener<JSONObject>() {
 
-            @Override
-            public void onResponse(JSONObject response) {
+			@Override
+			public void onResponse(JSONObject response) {
 
-                try {
+				try {
 
-                    JSONArray jsonArray = response.getJSONArray("study_schedule");
+					JSONArray jsonArray = response.getJSONArray("study_schedule");
 
-                    for (int i = 0; i < jsonArray.length(); i++) {
+					for (int i = 0; i < jsonArray.length(); i++) {
 
-                        JSONArray jsonArr2 = jsonArray.getJSONArray(i);
+						JSONArray jsonArr2 = jsonArray.getJSONArray(i);
 
-                        for (int j = 0; j < jsonArr2.length(); j++) {
+						for (int j = 0; j < jsonArr2.length(); j++) {
 
-                            JSONObject jsonObject = jsonArr2.getJSONObject(j);
-                            double duration = jsonObject.getDouble("duration");
-                            int courseID = jsonObject.getInt("course_id");
+							JSONObject jsonObject = jsonArr2.getJSONObject(j);
+							double duration = jsonObject.getDouble("duration");
+							int courseID = jsonObject.getInt("course_id");
 
-                            String start = jsonObject.getString("start");
-                            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-                            LocalDateTime startDateTime = LocalDateTime.parse(start, formatter);
-                            LocalDateTime currentDateTime = LocalDateTime.now();
+							String start = jsonObject.getString("start");
+							DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+							LocalDateTime startDateTime = LocalDateTime.parse(start, formatter);
+							LocalDateTime currentDateTime = LocalDateTime.now();
 
-                            if (currentDateTime.isAfter(startDateTime) && courseID == courseCode) {
+							if (currentDateTime.isAfter(startDateTime) && courseID == courseCode) {
 
-                                courseDurationList.add(duration);
+								courseDurationList.add(duration);
 
-                            }
+							}
 
-                        }
+						}
 
-                    }
+					}
 
-                } catch (JSONException e) {
+				} catch (JSONException e) {
 
-                    e.printStackTrace();
+					e.printStackTrace();
 
-                }
+				}
 
-            }
+			}
 
-        }, new Response.ErrorListener() {
+		}, new Response.ErrorListener() {
 
-            @Override
-            public void onErrorResponse(VolleyError error) {
+			@Override
+			public void onErrorResponse(VolleyError error) {
 
-                error.printStackTrace();
+				error.printStackTrace();
 
-            }
+			}
 
-        });
+		});
 
-        double courseTime = 0;
+		double courseTime = 0;
 
-        for (int i = 0; i < courseDurationList.size(); i++) {
+		for (int i = 0; i < courseDurationList.size(); i++) {
 
-            courseTime += courseDurationList.get(i);
+			courseTime += courseDurationList.get(i);
 
-        }
+		}
 
-        StudentAideSingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
-        return courseTime;
+		StudentAideSingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+		return courseTime;
 
-    }
-
+	}
+	*/
 }
